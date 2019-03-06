6ba07680a901deec95435316fe9b2be8639c7c63
Tibor Kiss <trks1970@gmail.com>
2019-03-07 00:27:52 +0100

    Initial commit.

---
+++ src/main/java/com/ioz/cocaine/Cocaine.java
@@ -0,0 +1,27 @@
+package com.ioz.cocaine;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.shell.jline.PromptProvider;
+
+@SpringBootApplication
+@EnableScheduling
+public class Cocaine
+{
+
+	public static void main( String[] args ) throws Exception
+	{
+		SpringApplication.run( Cocaine.class, args );
+	}
+
+	@Bean
+	public PromptProvider myPromptProvider()
+	{
+		return () -> new AttributedString( "cocaine:>", AttributedStyle.DEFAULT.foreground( AttributedStyle.YELLOW ) );
+	}
+}
